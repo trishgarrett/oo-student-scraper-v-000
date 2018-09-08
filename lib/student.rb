a4dc59b6db622b2af8f34fcd6f952fc05ab92d2b
Trisha Garrett <trisha.garrett@gmail.com>
2018-09-08 15:20:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    
   end
 
   def add_student_attributes(attributes_hash)
@@ -18,6 +18,6 @@ class Student
   end
 
   def self.all
-
+    @@all
   end
 end
